dd69d7a8d6799ab877cc1b713d9dfb9725dac75b
Raju NELLUR <rajunellur4@gmail.com>
2023-05-05 22:56:13 +0530

    First Spring boot project

---
+++ src/main/java/com/scaler/springbootproject01/SpringbootProject01Application.java
@@ -0,0 +1,13 @@
+package com.scaler.springbootproject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProject01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/springbootproject01/SpringbootProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.springbootproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
